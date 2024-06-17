182c17504c6103db565a3ca38187f6e3e3cdae35
Ayush Mishra (techy boy) <ayushmishra9799@gmail.com>
2024-06-17 14:16:20 +0530

    tesf

--- XModal-Form/src/main.jsx
+++ XModal-Form/src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import XModalForm from '../src/XModalForm'
+// import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <XModalForm />
   </React.StrictMode>,
 )
